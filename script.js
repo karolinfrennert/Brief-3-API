d29e3c0e4e0dca1ec9cb380aef41e040180d1e80
Agnes Kalström <62034881+agneskalstrom@users.noreply.github.com>
2020-11-19 13:15:45 +0100

    added script

---
+++ script.js
@@ -0,0 +1,39 @@
+const key = 'w5dwwGRRzGs6AgSz3teXnfI2FrdVGLdl'
+const url = 'https://app.ticketmaster.com/discovery/v2/events'
+const start = 'startDateTime'
+const startTime = 'T00:00:00Z'
+const end = 'endDateTime'
+const endTime = 'T23:59:00Z'
+const city = 'city'
+
+let imageEl = document.getElementById("image")
+let nameEl = document.getElementById("eventname")
+let dateEl = document.getElementById("eventdatetime")
+let venueEl = document.getElementById("eventvenue")
+let priceEl = document.getElementById("eventprice")
+
+let cityInput, dateInput, requestURL
+
+function getInput() {
+    cityInput = document.getElementById("cityname").value
+    dateInput = document.getElementById("date").value
+    requestURL = url.concat('?apikey=',key,'&locale=*','&',start,'=',dateInput,startTime,'&',end,'=',dateInput,endTime,'&',city,'=',cityInput)
+    showResult()
+}
+
+function showResult() {
+    fetch(requestURL) 
+    .then(response => response.json())
+    .then(data => {
+        let eventName = data._embedded.events[0].name
+        nameEl.innerHTML = eventName
+        let eventDateTime = data._embedded.events[0].dates.start.localDate + " @ " + data._embedded.events[0].dates.start.localTime
+        dateEl.innerHTML = eventDateTime
+        let eventVenue = data._embedded.events[0]._embedded.venues[0].address.line1
+        venueEl.innerHTML = eventVenue
+        let eventPrice = data._embedded.events[0].priceRanges[0].min + ' - ' + data._embedded.events[0].priceRanges[0].max + ' ' + data._embedded.events[0].priceRanges[0].currency
+        priceEl.innerHTML = eventPrice
+    })
+}
+
+document.getElementById("searchButton").addEventListener("click", getInput)
