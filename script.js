d882d9f82e1d517ee136b030bf04f161db706f48
Karolin Frennert <karolinfrennert@gmail.com>
2020-11-20 11:02:58 +0100

    Looping through each event + template

--- script.js
+++ script.js
@@ -11,44 +11,112 @@ let nameEl = document.getElementById("eventname")
 let dateEl = document.getElementById("eventdatetime")
 let venueEl = document.getElementById("eventvenue")
 let priceEl = document.getElementById("eventprice")
+let button = document.getElementById("button")
+
+
+
+
+
+
+
 
 let cityInput, dateInput, requestURL
 
 function getInput() {
-    cityInput = document.getElementById("cityname").value
-    dateInput = document.getElementById("date").value
-    budgetInput = document.getElementById("budget").value  
-    requestURL = url.concat('?apikey=',key,'&locale=*','&',start,'=',dateInput,startTime,'&',end,'=',dateInput,endTime,'&',city,'=',cityInput)
-    showResult()
+  cityInput = document.getElementById("cityname").value
+  dateInput = document.getElementById("date").value
+  budgetInput = document.getElementById("budget").value || 0 
+  requestURL = url.concat('?apikey=', key, '&locale=*', '&', start, '=', dateInput, startTime, '&', end, '=', dateInput, endTime, '&', city, '=', cityInput)
+  showResult()
 }
+
+const findMinMaxPrice = (prices) => {
+  const minPrices = prices.map(price =>  price.min)
+  const minPrice = minPrices.sort()[0]
+  const maxPrices = prices.map(price => price.max) 
+  const maxPrice = maxPrices.sort()[maxPrices.length - 1]   
+  return {minPrice, maxPrice}  
+}
+
 
 function showResult() {
-    fetch(requestURL) 
-    
+  fetch(requestURL)
     .then(response => response.json())
-    
     .then(data => {
-        console.log(budgetInput)
-        if (budgetInput >= data._embedded.events[0].priceRanges[0].min){
-        let eventName = data._embedded.events[0].name
-        nameEl.innerHTML = eventName
-        let eventDateTime = data._embedded.events[0].dates.start.localDate + " @ " + data._embedded.events[0].dates.start.localTime
-        dateEl.innerHTML = eventDateTime
-        let eventVenue = data._embedded.events[0]._embedded.venues[0].address.line1
-        venueEl.innerHTML = eventVenue
-        let eventPrice = data._embedded.events[0].priceRanges[0].min + ' - ' + data._embedded.events[0].priceRanges[0].max + ' ' + data._embedded.events[0].priceRanges[0].currency
-        priceEl.innerHTML = eventPrice
-        let imageUrl = data._embedded.events[0].images[0].url
-        imageEl.src = imageUrl;    
-        console.log(data);
-    }
-    else {
-        nameEl.innerHTML = "Sorry we couldn't find something within your budget";
-    } 
-    
-    
-    })
+      const events = data._embedded.events;
+      events.forEach(event => {
+        if (!event) return
+        
+        //Template content
+        const eventTemplate = document.querySelector("#eventTemplate")
+        const eventContent = document.querySelector("#eventContent")
+        const cloneTemplate = eventTemplate.content.cloneNode(true)
+
+        //Template elements
+        const image = cloneTemplate.querySelector("#image")
+        const eventName = cloneTemplate.querySelector("#eventname") 
+        const eventDateTime = cloneTemplate.querySelector("#eventdatetime")
+        const eventVenue = cloneTemplate.querySelector("#eventvenue")
+        const eventPrice = cloneTemplate.querySelector("#eventprice")
+        const templateButton = cloneTemplate.querySelector("#button")
+
+        const {priceRanges, name, dates, images, venues, address} = event;
+
+        if (!priceRanges && !name && !dates && !images && !venues && !address?.venues) return
+
+        console.log({priceRanges, name, dates, images})
+        const {minPrice, maxPrice} = findMinMaxPrice(priceRanges);
+        if (budgetInput >= minPrice) {   
+          
+          const imageUrl = event.images[0].url
+          image.src = imageUrl;
+          cloneTemplate.appendChild(image)
+
+          eventName.innerText = name
+          cloneTemplate.appendChild(eventName)        
+          
+          const startDateTime = `${start.localDate} @ ${start.localTime}`
+          eventDateTime.innerHTML = startDateTime
+          cloneTemplate.appendChild(eventDateTime)
+
+        /*console.log(venue)
+         const venue = venues.address.line1
+         eventVenue.innerHTML = venue*/
+
+          const price = minPrice + ' - ' + maxPrice + ' ' + priceRanges.currency
+          eventPrice.innerHTML = price
+          cloneTemplate.appendChild(eventPrice)
+
+          
+          eventContent.appendChild(cloneTemplate)
+        }
+
+      });
+
+      
+     
+  });
+  
 }
 
+
+
+  
+
 document.getElementById("searchButton").addEventListener("click", getInput)
+
+
+
+//Create button to change event
+/*const changeButton = document.createElement("button");
+changeButton.innerText = "Hey I'm a button";
+button.appendChild(changeButton);
+
+changeInnerText = () => {
+  changeButton.innerText = "Hey you clicked me";
+  changeButton.classList.add("button");
+}
+
+changeButton.addEventListener('click', changeInnerText);*/
+
 
