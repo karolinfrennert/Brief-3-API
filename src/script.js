97186046f65a88f104d1bda50abe306e5400a375
Cecilia Lobo <cecilialobo92@gmail.com>
2020-11-25 19:21:15 +0100

    Refactor the showInput function

--- src/script.js
+++ src/script.js
@@ -9,7 +9,7 @@ const swiper = new Swiper(".swiper-container", {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-},
+  },
 })
 
 const key = 'w5dwwGRRzGs6AgSz3teXnfI2FrdVGLdl'
@@ -22,102 +22,114 @@ const city = 'city'
 
 const BUTTON_TEXT = "Change event"
 
-let cityInput, dateInput, budgetInput, requestURL
-
 function getInput() {
-  cityInput = document.getElementById("cityname").value
-  dateInput = document.getElementById("date").value
-  budgetInput = document.getElementById("budget").value || 0 
-  requestURL = url.concat('?apikey=', key, '&locale=*', '&', start, '=', dateInput, startTime, '&', end, '=', dateInput, endTime, '&', city, '=', cityInput)
-  showResult()
+  let cityInput = document.getElementById("cityname").value;
+  let dateInput = document.getElementById("date").value;
+  
+  let budgetInput = document.getElementById("budget").value || 0;
+  let requestURL = url.concat('?apikey=', key, '&locale=*', '&', start, '=', dateInput, startTime, '&', end, '=', dateInput, endTime, '&', city, '=', cityInput);
+
+  showResult(budgetInput, requestURL);
 }
 
 const findMinMaxPrice = (prices) => {
-  const minPrices = prices.map(price =>  price.min)
+  const minPrices = prices.map(price => price.min)
   const minPrice = minPrices.sort()[0]
-  const maxPrices = prices.map(price => price.max) 
-  const maxPrice = maxPrices.sort()[maxPrices.length - 1]   
-  return {minPrice, maxPrice}  
+  const maxPrices = prices.map(price => price.max)
+  const maxPrice = maxPrices.sort()[maxPrices.length - 1]
+  return { minPrice, maxPrice }
 }
 
+async function convertEventsToArray(data) {
+  const events = [];
 
-async function showResult() {
-  await fetch(requestURL)
-    .then(response => response.json())
-    .then(data => {
-      const checkArray = data.page.totalElements;
-      if(checkArray !== 0) {
-      const events = data._embedded.events;
-      events.forEach(event => {
-        if (!event) return
-        
-        //Template content
-        const eventTemplate = document.querySelector("#eventTemplate")
-        const eventContent = document.querySelector("#eventContent")        
-        const cloneTemplate = eventTemplate.content.cloneNode(true)
+  if (data._embedded && data._embedded.events) {
+    data._embedded.events.forEach(singleEvent => {
+      if (!singleEvent) return;
 
-        //Template elements
-        const image = cloneTemplate.querySelector("#image")
-        const eventCard = cloneTemplate.querySelector(".eventCard")
-        const eventName = cloneTemplate.querySelector("#eventname") 
-        const eventDateTime = cloneTemplate.querySelector("#eventdatetime")
-        const eventVenue = cloneTemplate.querySelector("#eventvenue")
-        const eventPrice = cloneTemplate.querySelector("#eventprice")
-        const templateButton = cloneTemplate.querySelector("#button")   
-             
+      const { priceRanges, name, dates, images, _embedded } = singleEvent;
+      if (!priceRanges || !name || !dates || !images || !_embedded) return;
 
-        
-        const {priceRanges, name, dates, images, _embedded, address} = event;
+      events.push(singleEvent);
+    });
+  }
 
-        if (!priceRanges && !name && !dates && !images && !_embedded) return        
-        const {minPrice, maxPrice} = findMinMaxPrice(priceRanges)
+  return events;
+}
 
-        if (budgetInput >= minPrice) {   
-          
-          const imageUrl = event.images[0].url
-          image.src = imageUrl;
-          eventCard.appendChild(image)
+async function filterEventsByBudget(events, budget) {
+  const eventsInTheBudget = [];
 
-          eventName.innerText = name
-          eventCard.appendChild(eventName)        
-          
-          const startDateTime = `${dates.start.localDate} @ ${dates.start.localTime}`
-          eventDateTime.innerHTML = startDateTime
-          eventCard.appendChild(eventDateTime)
+  events.forEach(singleEvent => {
+    const { minPrice } = findMinMaxPrice(singleEvent.priceRanges);
 
-        
-         const venue = _embedded.venues[0].name
-         eventVenue.innerHTML = venue
-         eventCard.appendChild(eventVenue)
-
-          const price = minPrice + ' - ' + maxPrice + ' ' + priceRanges[0].currency
-          eventPrice.innerHTML = price
-          eventCard.appendChild(eventPrice)
-
-          templateButton.innerHTML = BUTTON_TEXT
-          
-          eventCard.appendChild(templateButton)          
-          cloneTemplate.appendChild(eventCard)
-          eventContent.appendChild(cloneTemplate)
-
-        }
-    
-      });  
-    } else {
-      getWeatherInput();
+    if (minPrice > budget) {
+      return;
     }
+
+    return eventsInTheBudget.push(singleEvent);
   });
 
-
-
-  swiper.update()
-  
-
-  
+  return eventsInTheBudget;
 }
 
+async function presentOnScreen(eventsInTheBudget) {
+  if (eventsInTheBudget.length <= 0) {
+    getWeatherInput();
+  } else {
+    eventsInTheBudget.forEach(eventToPresent => {
+      const { priceRanges, name, dates, images, _embedded } = eventToPresent;
+      const { minPrice, maxPrice } = findMinMaxPrice(priceRanges);
 
- 
+      const eventTemplate = document.querySelector("#eventTemplate");
+      const eventContent = document.querySelector("#eventContent");
+      const cloneTemplate = eventTemplate.content.cloneNode(true);
+
+      const image = cloneTemplate.querySelector("#image");
+      const eventCard = cloneTemplate.querySelector(".eventCard");
+      const eventName = cloneTemplate.querySelector("#eventname");
+      const eventDateTime = cloneTemplate.querySelector("#eventdatetime");
+      const eventVenue = cloneTemplate.querySelector("#eventvenue");
+      const eventPrice = cloneTemplate.querySelector("#eventprice");
+      const templateButton = cloneTemplate.querySelector("#button");
+
+      const imageUrl = images[0].url;
+      image.src = imageUrl;
+      eventCard.appendChild(image);
+
+      eventName.innerText = name;
+      eventCard.appendChild(eventName);
+
+      const startDateTime = `${dates.start.localDate} @ ${dates.start.localTime}`;
+      eventDateTime.innerHTML = startDateTime;
+      eventCard.appendChild(eventDateTime);
+
+      const venue = _embedded.venues[0].name;
+      eventVenue.innerHTML = venue;
+      eventCard.appendChild(eventVenue);
+
+      const price = minPrice + ' - ' + maxPrice + ' ' + priceRanges[0].currency;
+      eventPrice.innerHTML = price;
+      eventCard.appendChild(eventPrice);
+
+      templateButton.innerHTML = BUTTON_TEXT;
+
+      eventCard.appendChild(templateButton);
+      cloneTemplate.appendChild(eventCard);
+      eventContent.appendChild(cloneTemplate);
+    });
+  }
+}
+
+async function showResult(budget, requestURL) {
+  await fetch(requestURL)
+    .then(response => response.json())
+    .then(rawDataAsJSON => convertEventsToArray(rawDataAsJSON))
+    .then(events => filterEventsByBudget(events, budget))
+    .then(eventsInTheBudget => presentOnScreen(eventsInTheBudget));
+
+  swiper.update();
+}
 
 document.getElementById("searchButton").addEventListener("click", getInput)
 
